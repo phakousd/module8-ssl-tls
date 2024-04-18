38156c59e9e0547f73c92ae23ac008a3c7a446a8
phakousd <phakousd@my.canisius.edu>
2024-04-17 21:25:50 -0400

    Finally finished it, I had to reclone the project to fix the glitch and redo the .java file.

--- src/main/java/edu/canisius/cyb600/module8/X509AuthenticationServer.java
+++ src/main/java/edu/canisius/cyb600/module8/X509AuthenticationServer.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+
 @SpringBootApplication
 public class X509AuthenticationServer {
     @Bean
